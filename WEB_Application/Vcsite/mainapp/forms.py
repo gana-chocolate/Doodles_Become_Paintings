84b1944065218272eb9765ecaae567732d764f6a
JangHyukJin <wkdgurwls00@naver.com>
2019-06-10 15:48:53 +0900

    add form.py

---
+++ WEB_Application/Vcsite/mainapp/forms.py
@@ -0,0 +1,31 @@
+'''from django import forms
+
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()'''
+
+from django import forms
+
+from .models import UploadFileModel
+from .models import Post
+
+class UploadFileForm(forms.ModelForm):
+    class Meta:
+        model = UploadFileModel
+        fields = ('title', 'file')
+
+    def __init__(self, *args, **kwargs):
+        super(UploadFileForm, self).__init__(*args, **kwargs)
+        self.fields['file'].required = False
+
+class FileFieldForm(forms.Form):
+    file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+class UploadFileForm2(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('profile_pic', 'photo')
+
+    def __init__(self, *args, **kwargs):
+        super(UploadFileForm2, self).__init__(*args, **kwargs)
+        self.fields['profile_pic'].required = False
