fd6ca19448c039a6b9cb58b6b65f82c7859080c3
JangHyukJin <wkdgurwls00@naver.com>
2019-06-10 15:50:34 +0900

    set urls in django

--- WEB_Application/Vcsite/mainapp/urls.py
+++ WEB_Application/Vcsite/mainapp/urls.py
@@ -1,6 +1,19 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+app_name = 'mainapp'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('Upload/', views.upload_file, name='upload'),
+    path('Download/', views.download, name='download'),
+    path('Send/', views.send, name='send'),
+    path('Send2/', views.send2, name='send2'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
