f1a5e33a2404172bc52d8f1aa08fa77c9998eb2b
JangHyukJin <wkdgurwls00@naver.com>
2019-06-10 15:53:18 +0900

    add bootstrap.js

--- WEB_Application/Vcsite/mainapp/static/mainapp/vendor/bootstrap/js/bootstrap.js
+++ WEB_Application/Vcsite/mainapp/static/mainapp/vendor/bootstrap/js/bootstrap.js
@@ -4432,4 +4432,16 @@
   Object.defineProperty(exports, '__esModule', { value: true });
 
 }));
+$(document).ready(function(){
+  var fileTarget = $('.filebox .upload-hidden');
+  fileTarget.on('change', function(){
+    if(window.FileReader){
+      var filename = $(this)[0].files[0].name;
+    }
+    else {
+      var filename = $(this).val().split('/').pop().split('\\').pop();
+    }
+    $(this).siblings('.upload-name').val(filename);
+  });
+});
 //# sourceMappingURL=bootstrap.js.map
